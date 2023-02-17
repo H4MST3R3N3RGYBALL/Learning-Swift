b9caa5bd5b5ebe15eadace61d0931d45f75b9755
H4MST3R <53465855+H4MST3R3N3RGYBALL@users.noreply.github.com>
2023-02-17 18:28:02 -0500

    Adding Starting Point for Better Rest

---
+++ BetterRest/BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by h4mst3r on 2/17/23.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
