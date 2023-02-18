e077ceb8ad2b3b0786d1d7f29313a616d364c7a5
H4MST3R <53465855+H4MST3R3N3RGYBALL@users.noreply.github.com>
2023-02-18 17:24:42 -0500

    Adding prediction to the code

--- BetterRest/BetterRest/ContentView.swift
+++ BetterRest/BetterRest/ContentView.swift
@@ -6,11 +6,14 @@
 //
 
 import SwiftUI
+import CoreML
 struct ContentView: View {
     @State private var sleepAmount = 8.0
     @State private var wakeUp = Date.now
     @State private var coffeeAmount = 1
-    
+    @State private var alertTitle = ""
+    @State private var alertMessage = ""
+    @State private var showingAlert = false
     var body: some View {
         VStack {
             Text("When do you want to wake up?")
@@ -36,7 +39,22 @@ struct ContentView: View {
         }
     }
     func calculateBedTime() {
-        
+        do {
+            let config = MLModelConfiguration()
+            let model = try SleepCalculator(configuration: config)
+            // Converting the date compoenets to a double
+            let components = Calendar.current.dateComponents([.hour, .minute], from: wakeUp)
+            let hour = (components.hour ?? 0) * 60 * 60
+            let minute = (components.minute ?? 0) * 60
+            
+            let prediction = try model.prediction(wake: Double(hour + minute), estimatedSleep: sleepAmount, coffee: Double(coffeeAmount))
+            
+            let sleepTime = wakeUp - prediction.actualSleep
+            
+        }
+        catch {
+            
+        }
     }
 }
 
